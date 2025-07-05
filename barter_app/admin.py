0b51f142a532ff9493c0bade73aec2a9e014a54d
baevtimofey <tim-baev@yandex.ru>
2025-07-05 17:41:18 +0400

    feat: register models into admin panel

--- barter_app/admin.py
+++ barter_app/admin.py
@@ -1,1 +1,7 @@
-# Register your models here.
+from django.contrib import admin
+
+from .models import Ad, Category, ExchangeProposal
+
+admin.site.register(Ad)
+admin.site.register(Category)
+admin.site.register(ExchangeProposal)
